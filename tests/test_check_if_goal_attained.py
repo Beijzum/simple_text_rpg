cc14d01045b040b81c2059db1499bc6e9df566c3
Jason Chow <jchow149@my.bcit.ca>
2023-12-07 23:27:54 -0800

    Clean up project a bit

--- tests/test_check_if_goal_attained.py
+++ tests/test_check_if_goal_attained.py
@@ -3,20 +3,20 @@ Jason Chow
 A00942129
 """
 from unittest import TestCase
-from game import check_if_goal_attained
+from game import check_win_condition
 
 
-class TestCheckIfGoalAttained(TestCase):
+class TestCheckWinCondition(TestCase):
     def test_goal_attained_true(self):
         board = {(1, 1): "Whatever Room", (2, 2): "Chocolate Room"}
         character = {"X-coordinate": 2, "Y-coordinate": 2, "Current HP": 5}
-        test = check_if_goal_attained(board, character)
+        test = check_win_condition(board, character)
         expected = True
         self.assertEqual(expected, test)
 
     def test_goal_attained_false(self):
         board = {(1, 1): "Whatever Room", (2, 2): "Chocolate Room"}
         character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 5}
-        test = check_if_goal_attained(board, character)
+        test = check_win_condition(board, character)
         expected = False
         self.assertEqual(expected, test)
