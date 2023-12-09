4fc2db32ebc3fd13419c92423aba584a60210003
Jason Chow <jchow149@my.bcit.ca>
2023-12-09 15:25:31 -0800

    Add docstrings and doctests to utility.py

--- utility.py
+++ utility.py
@@ -6,11 +6,30 @@ def clear():
     Clear the terminal screen for better readability.
 
     This function will use "cls" for a Windows OS, else it will use "clear".
+
+    :param: N/A
+    :precondition: user must trigger an event
+    :precondition: user must be using a terminal
+    :postcondition: terminal screen will be cleared
+    :return: a cleared terminal screen
+
+    >>> clear()
+
     """
     os.system("cls" if os.name == "nt" else "clear")
 
 
 def draw_box(message: str) -> str:
+    """
+    Draw a box around a message.
+
+    This function will draw a box around a message using unicode characters. It will be used for special events.
+
+    :param message: a string
+    :precondition: message must be a string
+    :postcondition: prints a message enclosed in a box
+    :return: a string representing the message enclosed in a box.
+    """
     length = len(message) + 4
     top_border = "+" + "\u2594" * length + "+"
     bottom_border = "+" + "\u2581" * length + "+"
@@ -20,4 +39,15 @@ def draw_box(message: str) -> str:
 
 
 def draw():
+    """
+    Draw a horizontal line.
+
+    :param: N/A
+    :precondition: user must trigger an event
+    :postcondition: prints a horizontal line
+    :return: a string representing a horizontal line
+
+    >>> draw()
+    xX------------------------------------------------------Xx
+    """
     print("xX------------------------------------------------------Xx")
