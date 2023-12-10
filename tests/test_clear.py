2c8e02a9fd087ceb8c3f102b969dfe8c1981ac18
Jason Chow <jchow149@my.bcit.ca>
2023-12-09 21:15:10 -0800

    Add test_clear.py

---
+++ tests/test_clear.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from unittest.mock import patch
+from utility import clear
+
+
+# https://itecnote.com/tecnote/python-the-best-way-to-mock-os-system-for-unit-test-pytest/
+
+class TestClear(TestCase):
+    @patch('os.system')
+    @patch('os.name', 'nt')
+    def test_clear_windows(self, mock_system):
+        clear()
+        mock_system.assert_called_once_with('cls')
+
+    @patch('os.system')
+    @patch('os.name', 'posix')
+    def test_clear_linux(self, mock_system):
+        clear()
+        mock_system.assert_called_once_with('clear')
