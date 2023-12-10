5192c61c53ba42af1dae6cd9037ee734b9713fd9
Jason Chow <jchow149@my.bcit.ca>
2023-12-09 18:37:44 -0800

    Add test_learn_ability.py

---
+++ tests/test_learn_ability.py
@@ -0,0 +1,78 @@
+import io
+from unittest import TestCase
+from unittest.mock import patch
+from character import learn_ability
+
+
+class TestLearnAbility(TestCase):
+    def test_learn_ability_multi_strike(self):
+        character = {
+            "Level": 3,
+            "Abilities": {"1": {"Name": "Power Strike", "Power": 2, "AP Cost": 1}},
+        }
+
+        learn_ability(character)
+        actual = character
+        expected = {
+            "Abilities": {"1": {"Name": "Power Strike", "Power": 2, "AP Cost": 1},
+                          "2": {"Name": "Multi-Strike", "Power": 2, "AP Cost": 2}},
+            "Level": 3
+        }
+        self.assertEqual(actual, expected)
+
+    def test_learn_ability_holy_strike(self):
+        character = {
+            "Level": 5,
+            "Abilities": {"1": {"Name": "Power Strike", "Power": 2, "AP Cost": 1},
+                          "2": {"Name": "Multi-Strike", "Power": 2, "AP Cost": 2}},
+        }
+
+        learn_ability(character)
+        actual = character
+        expected = {
+            "Abilities": {"1": {"Name": "Power Strike", "Power": 2, "AP Cost": 1},
+                          "2": {"Name": "Multi-Strike", "Power": 2, "AP Cost": 2},
+                          "3": {"Name": "Holy Strike", "Power": 7, "AP Cost": 3}},
+            "Level": 5
+        }
+        self.assertEqual(actual, expected)
+
+    def test_learn_ability_ultimate_strike(self):
+        character = {
+            "Level": 7,
+            "Abilities": {"1": {"Name": "Power Strike", "Power": 2, "AP Cost": 1},
+                          "2": {"Name": "Multi-Strike", "Power": 2, "AP Cost": 2},
+                          "3": {"Name": "Holy Strike", "Power": 7, "AP Cost": 3}},
+        }
+
+        learn_ability(character)
+        actual = character
+        expected = {
+            "Abilities": {"1": {"Name": "Power Strike", "Power": 2, "AP Cost": 1},
+                          "2": {"Name": "Multi-Strike", "Power": 2, "AP Cost": 2},
+                          "3": {"Name": "Holy Strike", "Power": 7, "AP Cost": 3},
+                          "4": {"Name": "Ultimate Strike", "Power": 10, "AP Cost": 5}},
+            "Level": 7
+        }
+        self.assertEqual(actual, expected)
+
+    def test_learn_ability_too_long(self):
+        character = {
+            "Level": 10,
+            "Abilities": {"1": {"Name": "Power Strike", "Power": 2, "AP Cost": 1},
+                          "2": {"Name": "Multi-Strike", "Power": 2, "AP Cost": 2},
+                          "3": {"Name": "Holy Strike", "Power": 7, "AP Cost": 3},
+                          "4": {"Name": "Ultimate Strike", "Power": 10, "AP Cost": 5}},
+        }
+
+        learn_ability(character)
+        actual = character
+        expected = {
+            "Abilities": {"1": {"Name": "Power Strike", "Power": 2, "AP Cost": 1},
+                          "2": {"Name": "Multi-Strike", "Power": 2, "AP Cost": 2},
+                          "3": {"Name": "Holy Strike", "Power": 7, "AP Cost": 3},
+                          "4": {"Name": "Ultimate Strike", "Power": 10, "AP Cost": 5},
+                          "5": {"Name": "You're Playing Too Long", "Power": 999, "AP Cost": 10}},
+            "Level": 10
+        }
+        self.assertEqual(actual, expected)
