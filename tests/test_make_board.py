5319e81777edca6cd8ea7e2844548f4cb194f622
Jason Chow <jchow149@my.bcit.ca>
2023-12-09 17:29:28 -0800

    Update make board and character. Removed other test

--- tests/test_make_board.py
+++ tests/test_make_board.py
@@ -8,41 +8,21 @@ from game import make_board
 
 
 class TestMakeBoard(TestCase):
-    @patch("random.choice", side_effect=["Empty Room"] * 8 + ["Chocolate Room"])
-    def test_make_board_3_by_3_same_integer(self, _):
+    @patch("random.choice", side_effect=["Empty Room"] * 2)
+    def test_make_board_3_by_3(self, _):
         rows = 3
         columns = 3
         expected = {
             (0, 0): "Starting Room",
             (0, 1): "Empty Room",
-            (0, 2): "Empty Room",
+            (0, 2): 'Winter Sanctum',
             (1, 0): "Empty Room",
-            (1, 1): "Empty Room",
-            (1, 2): "Empty Room",
-            (2, 0): "Empty Room",
-            (2, 1): "Empty Room",
-            (2, 2): "Chocolate Room",
+            (1, 1): 'Traveling Merchant',
+            (1, 2): 'Ice Guardian Room',
+            (2, 0): 'Inferno Lair',
+            (2, 1): 'Fire Guardian Room',
+            (2, 2): "Final Room",
         }
-
-        test = make_board(rows, columns)
-        self.assertEqual(expected, test)
-
-    @patch("random.choice", side_effect=["Dark Room"] * 10 + ["Chocolate Room"])
-    def test_make_board_4_by_3_different_integer(self, _):
-        rows = 4
-        columns = 3
-        expected = {(0, 0): 'Starting Room',
-                    (0, 1): 'Dark Room',
-                    (0, 2): 'Dark Room',
-                    (1, 0): 'Dark Room',
-                    (1, 1): 'Dark Room',
-                    (1, 2): 'Dark Room',
-                    (2, 0): 'Dark Room',
-                    (2, 1): 'Dark Room',
-                    (2, 2): 'Dark Room',
-                    (3, 0): 'Dark Room',
-                    (3, 1): 'Dark Room',
-                    (3, 2): 'Chocolate Room'}
 
         test = make_board(rows, columns)
         self.assertEqual(expected, test)
--- tests/test_make_character.py
+++ tests/test_make_character.py
@@ -8,6 +8,28 @@ from game import make_character
 
 class TestMakeCharacter(TestCase):
     def test_make_character(self):
-        expected = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5}
+        expected = {
+            'X-coordinate': 0,
+            'Y-coordinate': 0,
+            'Max HP': 10,
+            'Current HP': 10,
+            'Max AP': 5,
+            'Ability Points': 5,
+            'Attack': 2,
+            'Defence': 0,
+            'Abilities': {
+                '1': {'Name': 'Power Strike', 'Power': 2, 'AP Cost': 1}
+            },
+            'Inventory': {
+                '1': {'Name': 'Bronze Sword', 'Power': 0, 'Type': 'Weapon'},
+                '2': {'Name': 'Clothes', 'Power': 0, 'Type': 'Armour'}
+            },
+            'Gold': 0,
+            'Level': 1,
+            'Experience Points': 0,
+            'EXP to Level Up': 10,
+            'Stronger Enemies': False
+        }
+
         test = make_character()
         self.assertEqual(expected, test)
